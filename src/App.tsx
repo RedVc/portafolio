d2e1375a799b3a8873e559eac86f281148025f24
RedVc <redvc13.03@gmail.com>
2026-01-20 14:37:14 -0600

    Limpieza de proyecto, instalacion de Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
     <>
-      {/* Rutas: */}
+      <BrowserRouter>
+        <Routes>
+          
+          {/* Plantilla principal y sus path */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
