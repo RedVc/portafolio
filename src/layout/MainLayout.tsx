5d01de54459ef5f270c87c9bb5555f28b1bf909f
RedVc <redvc13.03@gmail.com>
2026-01-20 15:44:55 -0600

    Construcción del MainLayout y aplicación de clases con Tailwind

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,11 +1,26 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
 
 const MainLayout = () => {
     return(
-        <div>
-            <main>
-                <Outlet/>
-            </main>
+        <div className="h-screen bg-black p-10 text-cyan-900" id="background">
+
+            <div className="border border-cyan-900 h-full p-16 flex flex-row justify-between">
+
+                {/* Sidebar */}
+                <aside>
+                    <Sidebar/>
+                </aside>
+
+                {/* Contenido */}
+                <main>
+                    <Outlet/>
+                </main>
+            </div>
+
+            <div className="text-sm m-1 font-bold">
+                © Redwin Valverde Castro
+            </div>
         </div>
     )
 }
